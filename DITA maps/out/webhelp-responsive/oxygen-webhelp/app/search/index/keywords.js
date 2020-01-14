d33e7d1d09fa652ec8c5626dfce63a98dfb9f0b0
Mihaela Ilovan <ilovan@ualberta.ca>
2020-01-14 12:14:38 -0700

    checked activism, religious, remuneration, research note, student body and styled

---
+++ DITA maps/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,13 @@
+define(function() {var keywords=[{w:"ACTIVISM",p:["p0"]},{w:"RELIGIOUS",p:["p1"]},{w:"REMUNERATION",p:["p2"]},{w:"RESEARCHNOTE",p:["p3"]},{w:"STUDENTBODY",p:["p4"]},{w:"STYLED",p:["p5"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2];
+ph["p3"]=[3];
+ph["p4"]=[4];
+ph["p5"]=[5];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
--- Documentation/OrlandoUserManual/oxygen-webhelp/app/search/index/keywords.js
+++ Documentation/OrlandoUserManual/oxygen-webhelp/app/search/index/keywords.js
@@ -1,8 +1,12 @@
-define(function() {var keywords=[{w:"ACTIVISM",p:["p0"]},{w:"REMUNERATION",p:["p1"]},{w:"Orlando",p:["p2"]},{w:"Glossary",p:["p2"]}];
+define(function() {var keywords=[{w:"ACTIVISM",p:["p0"]},{w:"RELIGIOUS",p:["p1"]},{w:"REMUNERATION",p:["p2"]},{w:"RESEARCHNOTE",p:["p3"]},{w:"STUDENTBODY",p:["p4"]},{w:"STYLED",p:["p5"]},{w:"Orlando",p:["p6"]},{w:"Glossary",p:["p6"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1];
-ph["p2"]=[2, 3];
+ph["p2"]=[2];
+ph["p3"]=[3];
+ph["p4"]=[4];
+ph["p5"]=[5];
+ph["p6"]=[6, 7];
      return {
          keywords: keywords,
          ph: ph
